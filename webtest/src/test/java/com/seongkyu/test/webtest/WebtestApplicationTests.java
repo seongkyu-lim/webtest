e1bd80098ce2f58c95667eb4f1507f97a957a3c3
limseongkyu <96x60812@naver.com>
2021-01-25 17:34:49 +0900

    first

---
+++ webtest/src/test/java/com/seongkyu/test/webtest/WebtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seongkyu.test.webtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
